a6b32c9f8055f6305caa7b2db5f6fe08352dbf70
DmitryNazarov <naz.dmtr@gmail.com>
2024-09-25 21:08:33 +0300

    Second tasks

---
+++ 023.js
@@ -0,0 +1,9 @@
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  const entreeValue = testObj["an entree"];
+  const drinkValue = testObj["the drink"];
