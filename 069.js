2d302e8ed8f704782ede2b6c0436ebded35e12d0
DmtrNz <naz.dmtr@gmail.com>
2024-12-23 22:13:11 +0300

    js tasks

---
+++ 069.js
@@ -0,0 +1,20 @@
+const account = {
+    name: "Bob"
+}
+
+const bonus = account
+
+const bread = {}
+
+const community = {
+    name: "Bob"
+}
+
+if ( bread=== bonus) {
+    console.log("Yo!!!")
+} else {
+    console.log("Hey!!!")
+}
+
+
+/*Какие переменные можно использовать вместо XXX, что бы в консоль вывелась строка "Hey"!!!"? Если их несколько, напишите через пробел.*/
---
+++ 070.js
@@ -0,0 +1,21 @@
+function persistence(num) {
+    let count = 0;
+
+    // Function to multiply the digits of a number
+    function multiplyDigits(n) {
+        return n.toString().split('').reduce((acc, digit) => acc * Number(digit), 1);
+    }
+
+    // Continue multiplying the digits until we have a single-digit number
+    while (num >= 10) {
+        num = multiplyDigits(num);
+        count++;
+    }
+
+    return count;
+}
+
+// Example usage:
+console.log(persistence(39));   // Output: 3
+console.log(persistence(999));  // Output: 4
+console.log(persistence(4));    // Output: 0
