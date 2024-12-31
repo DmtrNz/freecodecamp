612ed101ec3729652d6fec24274480e0b058218c
DmtrNz <naz.dmtr@gmail.com>
2024-12-31 22:18:58 +0300

    tests

---
+++ test2/076.test.js
@@ -0,0 +1,21 @@
+let users
+
+beforeEach(() => {
+    users = {
+        '101': { id: 101, name: 'Dimych' },
+        '3232312': { id: 3232312, name: 'Natasha' },
+        '1212': { id: 1212, name: 'Valera' },
+        '1': { id: 1, name: 'Katya' }
+    }
+})
+
+test('should update corresponding user', () => {
+    users['1'].name = 'Ekaterina'
+    delete users['1212']
+
+
+    expect(users['1'].name).toBe('Ekaterina');
+    expect(users['1']).toEqual({ id: 1, name: 'Ekaterina' })
+    expect(users['1212']).toBe(undefined) //аналогично записи ниже
+    expect(users['1212']).toBeUndefined()
+})
