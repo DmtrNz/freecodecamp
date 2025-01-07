a80ad02f94cac312225eab2f9280ea2caa3d1582
DmtrNz <naz.dmtr@gmail.com>
2025-01-07 21:21:49 +0300

    task js

---
+++ 084.js
@@ -0,0 +1,14 @@
+function getUniqueNumbers(arr) {
+    // Используем Set для удаления дубликатов
+    let uniqueSet = new Set(arr);
+
+    // Преобразуем Set обратно в массив
+    let uniqueArray = [...uniqueSet];
+
+    return uniqueArray;
+}
+
+// Пример использования
+let numbers = [1, 2, 2, 3, 4, 4, 5];
+let uniqueNumbers = getUniqueNumbers(numbers);
+console.log(uniqueNumbers); // Вывод: [1, 2, 3, 4, 5]
